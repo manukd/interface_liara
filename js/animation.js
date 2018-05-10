fa2174f98b08ff9ff768bcafcdac625164e2f6a5
Pierre Wenzel <pierre.wenzel@orange.fr>
2018-05-10 14:28:27 -0400

    porte pièces annimation OK

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,5 @@
 
-let motionPath = anime({
+let frigoOverture = anime({
     targets: '.cls-20',
     rotate: {
         value:[-45,-135]
@@ -9,4 +9,97 @@ let motionPath = anime({
     autoplay: false
 });
 
-document.querySelector('.frigoAnime').onclick = motionPath.restart;
+
+let frigoFermeture = anime({
+    targets: '.cls-20',
+    rotate: {
+        value:[-135,-45]
+    },
+    easing: 'linear',
+    duration: 750,
+    autoplay: false
+});
+
+let porteSalleDeConfO = anime({
+    targets: '.porteSalleConf',
+    rotate: {
+        value:[0,-90]
+    },
+    easing: 'linear',
+    duration: 750,
+    autoplay: false
+
+});
+
+let porteSalleDeConfF = anime({
+    targets: '.porteSalleConf',
+    rotate: {
+        value:[-90,0]
+    },
+    easing: 'linear',
+    duration: 750,
+    autoplay: false
+
+});
+
+let porteSalleExterieurO = anime({
+    targets: '.porteExterieur',
+    rotate: {
+        value:[0,-90]
+    },
+    easing: 'linear',
+    duration: 750,
+    autoplay: false
+
+});
+
+let porteSalleExterieurF = anime({
+    targets: '.porteExterieur',
+    rotate: {
+        value:[-90,0]
+    },
+    easing: 'linear',
+    duration: 750,
+    autoplay: false
+
+});
+
+let porteSalleDeBainO = anime({
+    targets: '.porteSalleDeBain',
+    rotate: {
+        value:[0,-90]
+    },
+    easing: 'linear',
+    duration: 750,
+    autoplay: false
+
+});
+
+let porteSalleDeBainF = anime({
+    targets: '.porteSalleDeBain',
+    rotate: {
+        value:[-90,0]
+    },
+    easing: 'linear',
+    duration: 750,
+    autoplay: false
+
+});
+
+
+document.querySelector('.frigoAnimeO').onclick = frigoOverture.restart;
+
+document.querySelector('.frigoAnimeF').onclick = frigoFermeture.restart;
+
+document.querySelector('.porteConfO').onclick = porteSalleDeConfO.restart;
+
+document.querySelector('.porteConfF').onclick = porteSalleDeConfF.restart;
+
+document.querySelector('.porteExterieurOB').onclick = porteSalleExterieurO.restart;
+
+document.querySelector('.porteExterieurFB').onclick = porteSalleExterieurF.restart;
+
+document.querySelector('.porteSalleDeBainOB').onclick = porteSalleDeBainO.restart;
+
+document.querySelector('.porteSalleDeBainFB').onclick = porteSalleDeBainF.restart;
+
