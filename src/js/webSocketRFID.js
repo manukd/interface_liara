c7ba9d16c089bf4f1a15411c96c2b4993c4f8f0c
Pierre Wenzel <pierre.wenzel@orange.fr>
2018-05-24 15:13:34 -0400

    mise en place des premier tests RFID

---
+++ src/js/webSocketRFID.js
@@ -0,0 +1,21 @@
+const socketRFID = new WebSocket('ws://172.24.24.2:6094');
+
+socketRFID.onmessage = function (event) {
+    let d = JSON.parse(event.data);
+    for (let i = 0; i < d.lst_RFIDPosition.length; i++) {
+        switch (d.lst_RFIDPosition[i].ID) {
+            case "00000000001B2A4601001839" :
+                let animeTasse= anime({
+                    targets: '.tasse',
+                    easing: 'linear',
+                    translateX: ((d.lst_RFIDPosition[i].Coord.X)*559)/1150,
+                    translateY: ((d.lst_RFIDPosition[i].Coord.Y)*267)/400,
+                    opacity: 1
+                });
+                break;
+            default:
+                break;
+        }
+    }
+ 
+};
