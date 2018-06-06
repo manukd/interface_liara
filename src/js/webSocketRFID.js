ce2e6f2cb53be287c53aa10584a89ee8a6bdc24d
Pierre Wenzel <pierre.wenzel@orange.fr>
2018-06-06 06:32:33 -0400

    mise en place de npm

--- src/js/webSocketRFID.js
+++ src/js/webSocketRFID.js
@@ -1,6 +1,6 @@
 let path_socket_objets = "";
-function changerWebsocketObjets()
-{
+
+function changerWebsocketObjets() {
     alertify
         .defaultValue("")
         .prompt("Entrer l'adresse du websocket qui envoie la position des objets",
@@ -8,42 +8,42 @@ function changerWebsocketObjets()
 
                 // The click event is in the event variable, so you can use it here.
                 ev.preventDefault();
-                document.querySelector('#adresse_objets').innerHTML=val;
+                document.querySelector('#adresse_objets').innerHTML = val;
                 // The value entered is availble in the val variable.
                 alertify.success("Adresse validée");
 
-            }, function(ev) {
+            }, function (ev) {
 
                 // The click event is in the event variable, so you can use it here.
                 ev.preventDefault();
                 alertify.error("Adresse annulée");
             });
 }
-function setPathObjets(path)
-{
+
+function setPathObjets(path) {
     path_socket_objets = path;
 }
-function closeSocket()
-{
+
+function closeSocket() {
     socket.close();
-    path_socket_objets="";
+    path_socket_objets = "";
 }
-function websocketObjetsTurnOn()
-{
+
+function websocketObjetsTurnOn() {
     try {
-        let socketRFID = new WebSocket('ws://'+path_socket_objets);
+        let socketRFID = new WebSocket('ws://' + path_socket_objets);
         alertify.success("Websocket objet connecté");
 
         socketRFID.onmessage = function (event) {
             let d = JSON.parse(event.data);
-            for (let i = 0; i<d.lst_RFIDPosition.length; i++) {
-                if(tabObjetRFID.indexOf(d.lst_RFIDPosition[i].TagName) > -1) {
+            for (let i = 0; i < d.lst_RFIDPosition.length; i++) {
+                if (tabObjetRFID.indexOf(d.lst_RFIDPosition[i].TagName) > -1) {
                     document.getElementById(d.lst_RFIDPosition[i].TagName).style.opacity = '1';
                     anime({
-                        targets: '#'+d.lst_RFIDPosition[i].TagName,
+                        targets: '#' + d.lst_RFIDPosition[i].TagName,
                         easing: 'linear',
-                        translateX: ((d.lst_RFIDPosition[i].Coord.X)*559)/1150,
-                        translateY: 267-(((d.lst_RFIDPosition[i].Coord.Y)*267)/400)
+                        translateX: ((d.lst_RFIDPosition[i].Coord.X) * 559) / 1150,
+                        translateY: 267 - (((d.lst_RFIDPosition[i].Coord.Y) * 267) / 400)
                     });
                 }
             }
